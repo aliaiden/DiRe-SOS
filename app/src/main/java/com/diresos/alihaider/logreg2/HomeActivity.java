71b3ff4884f461395c9c58e8923b9687290a80fa
Ali Haider <aliaiden@yahoo.com>
2016-09-13 21:45:46 +0500

    php scripts all clear. Still register working fine only.

--- app/src/main/java/com/diresos/alihaider/logreg2/HomeActivity.java
+++ app/src/main/java/com/diresos/alihaider/logreg2/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.diresos.alihaider.logreg2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +12,19 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class HomeActivity extends AppCompatActivity {
-    Button dashboard,sendNot;
+    Button dashboard, sendNot;
+    String app_server_url = "http://192.168.0.108/fcmtest/fcm_insert.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +35,51 @@ public class HomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        dashboard=(Button) findViewById(R.id.bDashboard);
-
+        dashboard = (Button) findViewById(R.id.bDashboard);
+        sendNot = (Button) findViewById(R.id.bSendNot);
 
 
         dashboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(HomeActivity.this, MainActivity.class);
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
                 startActivity(i);
+            }
+        });
+
+        sendNot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.FCM_PREF), Context.MODE_PRIVATE);
+                final String token = sharedPreferences.getString(getString(R.string.FCM_TOKEN), "");
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, app_server_url,
+                        new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }
+                        , new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("fcm_token", token);
+
+                        return params;
+                    }
+                };
+                MySingleton.getInstance(HomeActivity.this).addToRequestQueue(stringRequest);
+
             }
         });
 
     }
 
-    }
+}
